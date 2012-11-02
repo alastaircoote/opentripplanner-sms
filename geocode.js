83957fe093fabd0e6f81c601d0f70ca1a01bfa36
Alastair Coote <alastair.coote@gmail.com>
2012-11-02 16:03:50 -0400

    Weird geocode issue on server

--- geocode.js
+++ geocode.js
@@ -5,7 +5,7 @@ var download = require("./download.js");
 module.exports = function(address,retFunc) {
 	//address += ", New York";
 	bounds = "40.589, -74.054|40.796, -73.783"
-	download("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&components=administrative_area:New York&address=" + encodeURIComponent(address), function(data) {
+	download("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&components=administrative_area:New+York&address=" + encodeURIComponent(address), function(data) {
 		retFunc(data);
 	})
 }
